437a41b43906eb60112992a51c36c22010d9e1a6
ljc0424 <1979328077@qq.com>
2023-07-28 18:10:46 +0800

    feat: show chat text while trying to unlock research

--- src/main/java/io/taraxacum/finaltech/core/listener/ResearchListener.java
+++ src/main/java/io/taraxacum/finaltech/core/listener/ResearchListener.java
@@ -2,7 +2,7 @@ package io.taraxacum.finaltech.core.listener;
 
 import io.github.thebusybiscuit.slimefun4.api.events.PlayerPreResearchEvent;
 import io.github.thebusybiscuit.slimefun4.api.researches.Research;
-import io.taraxacum.finaltech.core.interfaces.SpecialResearch;
+import io.taraxacum.finaltech.core.dto.SpecialResearch;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,8 +13,10 @@ public class ResearchListener implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onResearchStart(PlayerPreResearchEvent playerPreResearchEvent) {
         Research research = playerPreResearchEvent.getResearch();
-        Player player = playerPreResearchEvent.getPlayer();
         if (research instanceof SpecialResearch specialResearch) {
+            Player player = playerPreResearchEvent.getPlayer();
+            String[] showText = specialResearch.getChatText(player);
+            player.sendMessage(showText);
             if(specialResearch.canResearch(player)) {
                 specialResearch.afterResearch(player);
             } else {
