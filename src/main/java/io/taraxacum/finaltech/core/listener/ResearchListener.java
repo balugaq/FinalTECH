7ab4408a65c5859379c77151d5bd5d083f865803
ecro.fun <ecro.fun@gmail.com>
2023-07-31 02:25:46 +0800

    feat: send message while researching

--- src/main/java/io/taraxacum/finaltech/core/listener/ResearchListener.java
+++ src/main/java/io/taraxacum/finaltech/core/listener/ResearchListener.java
@@ -2,7 +2,7 @@ package io.taraxacum.finaltech.core.listener;
 
 import io.github.thebusybiscuit.slimefun4.api.events.PlayerPreResearchEvent;
 import io.github.thebusybiscuit.slimefun4.api.researches.Research;
-import io.taraxacum.finaltech.core.interfaces.SpecialResearch;
+import io.taraxacum.finaltech.core.dto.SpecialResearch;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -15,6 +15,7 @@ public class ResearchListener implements Listener {
         Research research = playerPreResearchEvent.getResearch();
         Player player = playerPreResearchEvent.getPlayer();
         if (research instanceof SpecialResearch specialResearch) {
+            player.sendMessage(specialResearch.getChatText(player));
             if(specialResearch.canResearch(player)) {
                 specialResearch.afterResearch(player);
             } else {
